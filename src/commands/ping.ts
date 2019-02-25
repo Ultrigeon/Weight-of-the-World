8291f066163e629a24feafee9df2453bc994ae54
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-02-25 19:33:31 +0100

    modified to make it an example

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,8 +1,6 @@
 import { Command } from "../Command"
 import { Client, Message } from "discord.js"
 
-let ping = new Command ("ping", function (bot: Client, message: Message, args: string) {
+module.exports = new Command ("ping", function (bot: Client, message: Message, args: string) {
     message.reply("pong!")
-})
-
-module.exports = ping 
+}) 
