c433c189bf5193cef124cd2447c093dbeed62fe4
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-02-25 19:43:53 +0100

    prettified

--- src/PassiveScript.ts
+++ src/PassiveScript.ts
@@ -1,24 +1,22 @@
-import { Client } from "discord.js"
+import { Client } from "discord.js";
 
-export type PassiveFunc = (bot: Client) => void
+export type PassiveFunc = (bot: Client) => void;
 
 export class PassiveScript {
-    
-    func: PassiveFunc
-    interval: number
-    
-    constructor(func: PassiveFunc, interval: number) {
-        this.func = func
-        this.interval = interval
-    }
-    
-    exec(bot: Client) {
-        var target_client: Client = bot
-        var target_script: PassiveScript = this
+  func: PassiveFunc;
+  interval: number;
 
-        setInterval(function() {
-            target_script.func(target_client)
-        }, this.interval)
-    }
-    
+  constructor(func: PassiveFunc, interval: number) {
+    this.func = func;
+    this.interval = interval;
+  }
+
+  exec(bot: Client) {
+    var target_client: Client = bot;
+    var target_script: PassiveScript = this;
+
+    setInterval(function() {
+      target_script.func(target_client);
+    }, this.interval);
+  }
 }
