622598c429b606b152eebb7991d3eb26dd0a5d2e
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-02-25 19:36:21 +0100

    prettified

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,6 +1,6 @@
-import { Command } from "../Command"
-import { Client, Message } from "discord.js"
+import { Command } from "../Command";
+import { Client, Message } from "discord.js";
 
-module.exports = new Command ("ping", function (bot: Client, message: Message, args: string) {
-    message.reply("pong!")
-}) 
+module.exports = new Command("ping", function(bot: Client, message: Message, args: string) {
+  message.reply("pong!");
+});
