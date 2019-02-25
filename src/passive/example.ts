ce45fc33a4b8a0bf9c7eefbfeeb495d75f67e43a
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-02-25 19:36:56 +0100

    prettified

--- src/passive/example.ts
+++ src/passive/example.ts
@@ -1,9 +1,9 @@
-import { PassiveScript, PassiveFunc } from "../PassiveScript"
-import { CommitGuide, get_commitresponse } from "../CommitGuide"
+import { PassiveScript, PassiveFunc } from "../PassiveScript";
+import { CommitGuide, get_commitresponse } from "../CommitGuide";
 
-let interval: number = 60*1000
+let interval: number = 60 * 1000;
 let script: PassiveFunc = function(bot: CommitGuide) {
-    console.log(`60k ms have passed, and I am ${bot.user.username}!`)
-}
+  console.log(`60k ms have passed, and I am ${bot.user.username}!`);
+};
 
-module.exports = new PassiveScript(script, interval)
+module.exports = new PassiveScript(script, interval);
