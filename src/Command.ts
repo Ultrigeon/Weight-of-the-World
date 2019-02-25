276d14dba6379e69fe5be9e7d61c84f141012464
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-02-25 19:39:49 +0100

    prettified

--- src/Command.ts
+++ src/Command.ts
@@ -1,29 +1,23 @@
-import { Client, Message } from "discord.js"
+import { Client, Message } from "discord.js";
 
-// export type CommandName = /^[A-Za-z0-9]+$/ ♥ please make it happen
-export type CustomCommand = (bot: Client, message: Message, args: string) => void
+export type CommandName = string
+export type CustomCommand = (
+  bot: Client,
+  message: Message,
+  args: string
+) => void;
 
 export class Command {
+  name: CommandName
+  func: CustomCommand;
 
-    _name: string // CommandName
-    func: CustomCommand
-
-    constructor(name: string, func: CustomCommand) {
-        this.name = name
-        this.func = func
-    }
-
-    set name(name: string) {
-        this._name = name
-    }
-
-    get name() {
-        return this._name
-    }
-
-    execute(bot: Client, message: Message, args: string) {
-        console.log(`Executing "${this.name}" with args: "${args}"`)
-        this.func(bot, message, args)
-    }
-
+  constructor(name: CommandName, func: CustomCommand) {
+    this.name = name;
+    this.func = func;
+  }
+  
+  execute(bot: Client, message: Message, args: string) {
+    console.log(`Executing "${this.name}" with args: "${args}"`);
+    this.func(bot, message, args);
+  }
 }
